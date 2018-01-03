e20c950f65528a8a0b254de86b9cef654d0da279
Bar Genish <genishbar@gmail.com>
2018-01-03 15:56:28 +0200

    New Project Have fun lil MF

---
+++ Time_In/Time_In/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Time_In.MvcApplication" Language="C#" %>
---
+++ Time_In/Time_In/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Time_In.Startup))]
+namespace Time_In
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
